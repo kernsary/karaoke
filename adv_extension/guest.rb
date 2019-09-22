b5e0b544442d40733844d51ed311c0f481bff7aa
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-09-22 18:35:00 +0100

    tested adv_extension guest has favourite song and can cheer

--- adv_extension/guest.rb
+++ adv_extension/guest.rb
@@ -1,14 +1,19 @@
 class Guest
 
-  attr_reader :name, :wallet
+  attr_reader :name, :wallet, :fav_song
 
-  def initialize(name, wallet)
+  def initialize(name, wallet, fav_song)
     @name = name
     @wallet = wallet
+    @fav_song = fav_song
   end
 
   def guest_pays(amount)
     @wallet -= amount
+  end
+
+  def guest_cheers
+    return "Ya dancer!"
   end
 
 end
--- adv_extension/specs/guest_spec.rb
+++ adv_extension/specs/guest_spec.rb
@@ -6,7 +6,7 @@ class GuestTest < MiniTest::Test
 
   def setup
 
-    @guest_1 = Guest.new("Frank", 100)
+    @guest_1 = Guest.new("Frank", 100, "My Way")
 
   end
 
@@ -21,6 +21,14 @@ class GuestTest < MiniTest::Test
   def test_can_take_money_from_guest
     @guest_1.guest_pays(10)
     assert_equal(90, @guest_1.wallet)
+  end
+
+  def test_get_fav_song
+    assert_equal("My Way", @guest_1.fav_song)
+  end
+
+  def test_guest_can_cheer
+    assert_equal("Ya dancer!", @guest_1.guest_cheers)
   end
 
 end
